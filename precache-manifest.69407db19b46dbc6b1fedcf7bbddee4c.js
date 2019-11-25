d233e4e49228ad972c2899089673f38d2ed4ecf2
zhangcandrew <zhangcandrew@gmail.com>
2019-11-24 19:22:37 -0500

    Updates

---
+++ precache-manifest.69407db19b46dbc6b1fedcf7bbddee4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8d43c1a9dabc552fbf616c967346a",
+    "url": "/nice/index.html"
+  },
+  {
+    "revision": "d51359671a76eb804ffa",
+    "url": "/nice/static/css/main.9bbf4f41.chunk.css"
+  },
+  {
+    "revision": "3bd9ec0f64e1f19ff7ad",
+    "url": "/nice/static/js/2.6b4c6e5e.chunk.js"
+  },
+  {
+    "revision": "d51359671a76eb804ffa",
+    "url": "/nice/static/js/main.3e7b859e.chunk.js"
+  },
+  {
+    "revision": "9d44653a5317cf27b387",
+    "url": "/nice/static/js/runtime-main.11a9287e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/nice/static/media/logo.25bf045c.svg"
+  }
+]);
